befdb79fc3085bf7cd57ef80385148e09ea96445
boo1ean <boo1ean0807@gmail.com>
2021-01-29 13:19:33 +0200

    Wire up detailed reports

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -1,34 +1,68 @@
 <template lang="pug">
 v-card
-	v-card-title Session result
+	v-card-title.text-center Session result
 	v-card-text
-		v-simple-table
-			template(v-slot:default)
-				tbody
-					tr
-						td Duration
-						td {{ r.duration | duration }}
-					tr
-						td Total binds
-						td {{ r.totalTasksCount }}
-					tr
-						td Completed
-						td {{ r.completedTasksCount }}
-					tr
-						td Failed
-						td {{ r.failedTasksCount }}
-					tr
-						td Wrong attempts
-						td {{ r.wrongAttempts }}
-					tr
-						td Avg. reaction time
-						td {{ r.avgReactionTime | reaction }}
-					tr
-						td Slowest keybind
-						td {{ r.maxReactionTimeBind.title }}
-					tr
-						td Fastest keybind
-						td {{ r.minReactionTimeBind.title }}
+		v-tabs(
+			v-model="tab"
+			background-color="transparent"
+			color="basil"
+			grow
+		)
+			v-tab(
+				key="summary"
+			) Summary
+			v-tab(
+				key="details"
+			) Details
+
+		v-tabs-items(v-model="tab")
+			v-tab-item(
+				key="summary"
+			)
+				v-simple-table
+					template(v-slot:default)
+						tbody
+							tr
+								td Duration
+								td {{ r.duration | duration }}
+							tr
+								td Total binds
+								td {{ r.totalTasksCount }}
+							tr
+								td Completed
+								td {{ r.completedTasksCount }}
+							tr
+								td Failed
+								td {{ r.failedTasksCount }}
+							tr
+								td Wrong attempts
+								td {{ r.wrongAttempts }}
+							tr
+								td Avg. reaction time
+								td {{ r.avgReactionTime | reaction }}
+							tr
+								td Slowest keybind
+								td {{ r.maxReactionTimeBind.title }}
+							tr
+								td Fastest keybind
+								td {{ r.minReactionTimeBind.title }}
+			v-tab-item(
+				key="details"
+			)
+				v-data-table(
+					:headers="detailsHeaders"
+					:items="tasks"
+					:items-per-page=100
+					hide-default-footer
+				)
+					template(v-slot:item.totalAttempts="{ item }")
+						span
+							span.green--text {{ item.completed.length }}
+							span /
+							span.orange--text {{ item.wrongAttempts }}
+							span /
+							span.red--text {{ item.failed.length }}
+
 		br
 		.text-center
 			v-btn(@click="() => $emit('save')").mr-2 Save result
@@ -42,6 +76,38 @@ v-card
 <script>
 export default {
 	props: ['r'],
+	data () {
+		return {
+			tab: 'summary',
+			detailsHeaders: [
+				{
+					text: 'Bind',
+					value: 'title',
+				},
+				{
+					text: 'Attempts',
+					value: 'totalAttempts',
+				},
+				{
+					text: 'Avg. reaction',
+					value: 'avgReactionTime',
+				},
+				{
+					text: 'Slowest reaction',
+					value: 'maxReactionTime',
+				},
+				{
+					text: 'Fastest reaction',
+					value: 'minReactionTime',
+				},
+			],
+		}
+	},
+	computed: {
+		tasks: function () {
+			return Object.values(this.r.taskById)
+		}
+	},
 	filters: {
 		duration (ms) {
 			return Math.round(ms / 1000) + ' seconds'
